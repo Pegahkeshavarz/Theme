f9caecefcdd4e83496d8d1b3e418714aa17a6764
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-03-24 15:18:40 -0700

    stable build of index.html: added modal

--- js/home.js
+++ js/home.js
@@ -1,22 +1,25 @@
 //Should swing the Logos on when the Logos appear in view **NOT WORKING**
 function onScreenLogo() {
+	//Current scrollTop() position
 	var currScrollTop = $(document).scrollTop();
 
+	//ScrollTop of surrounding <ul> && Height of the <ul>
 	var logoScrollTop = $('#companies').offset().top;
 	var logoHeight = Number($('#companies').css('height').slice(0,2));
 
+	//Height of the Window
 	var winHeight = $(window).height();
 
-	// console.log(currScrollTop);
-	// console.log(logoScrollTop);
-	// console.log(logoHeight);
-	// console.log(logoScrollTop+logoHeight);
-
-
-	if (currScrollTop+winHeight >= logoScrollTop+logoHeight) {
-		$('.cImage').addClass('animated bounce');
-	} else {
-		$('.cImage').removeClass('animated bounce');
+	for (var i = 0; i < $('.company').length; i++) {
+		//If current top + height of window (bottom of page) >=
+		//		scrollTop of surrounding <ul> + Height of <ul>
+		//	case true: 	add class --> bounces
+		//  case false: remove class--> refreshes the bounce
+		if (currScrollTop+winHeight >= logoScrollTop+logoHeight) {
+			$('.cImage:eq('+i+')').addClass('animated bounce').delay(500);
+		} else {
+			$('.cImage:eq('+i+')').removeClass('animated bounce');
+		}
 	}
 }
 function hoverSwing() {
